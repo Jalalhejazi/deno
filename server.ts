9643ca80f7efd8a8bead1007b38c5e62a4102886
Jalal Hejazi <jalal.hejazi@gmail.com>
2020-05-22 18:00:50 +0200

    deno training

---
+++ server.ts
@@ -0,0 +1,12 @@
+// deno run --allow-net http.ts
+import { serve } from "https://deno.land/std@0.52.0/http/server.ts"
+
+const PORT =  1234
+
+const s = serve({ port: PORT })
+
+console.log(`http://localhost:${PORT}/`);
+
+for await (const req of s) {
+  req.respond({ body: "Hello World from deno 2020\n" });
+}
---
+++ test.ts
@@ -0,0 +1,7 @@
+// deno run test.ts
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+
+assertEquals("hello", "hello");
+assertEquals("world", "world");
+
+console.log("Asserted! 🎉");
