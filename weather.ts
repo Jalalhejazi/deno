b1dbe9509e1dc46458d2965f9236a6ddf8c7b371
Jalal Hejazi <jalal.hejazi@gmail.com>
2020-05-22 15:40:05 +0000

    Added weather.ts

---
+++ weather.ts
@@ -0,0 +1,15 @@
+// deno run --allow-net weather.ts
+
+const url = 'http://www.henrikdemant.com/weather/realtime.txt'
+const result = await fetch(url);
+const data = await result.text()
+
+const temperaturArray  = data.split(/(\s+)/)
+const temperaturObject = {
+    Dato    			: temperaturArray[0],
+    Tid     			: temperaturArray[2],
+    temperatur 		: temperaturArray[4]
+}
+
+
+console.log(temperaturObject)
